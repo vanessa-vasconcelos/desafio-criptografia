539474228e1d4715bd0b2d6c6cc6c54741e0b04d
Vanessa <104469671+vanessa-vasconcelos@users.noreply.github.com>
2022-05-10 16:01:05 -0300

    Finalizei o alerta e criei a música

--- js/alert.js
+++ js/alert.js
@@ -1,15 +1,20 @@
-let msg_alerta = `
-<div class="alert-box">
+let msgAlerta = `
+<div class="alertBox" style="display:none;">
     <h1>Aviso</h1>
     <p>Protocolo incorreto, deseja converter o seu texto para o padrão?</p>
-    <button id="btn-alerta">Converter Texto</button>
+    <button id="btn-alerta" >Converter Texto</button>
 </div>
-`
-document.write(msg_alerta);
+`;
+document.write(msgAlerta);
 
-const btnAlerta = document.getElementById('#btn-alerta')
+const btnAlerta = document.getElementById('btn-alerta');
+const caixaAlerta = document.querySelector('.alertBox');
+const textArea = document.getElementById('criptografar');
 
-
-btnAlerta.addEventListener('click', function(){
-    const mensagem = textAreaDescript.value
-})
+btnAlerta.addEventListener('click', function () {
+	const mensagem = textArea.value;
+	const mensagemLower = mensagem.toLowerCase();
+	textArea.value = mensagemLower.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+	caixaAlerta.style.display = 'none';
+	textAreaCript.readOnly = false;
+});
--- js/copiar.js
+++ js/copiar.js
@@ -1,8 +1,8 @@
 const botaoCopiar = document.getElementById('btn-copy');
 
-botaoCopiar.addEventListener('click', function() {
-    textAreaDescript.select();
-    textAreaDescript.setSelectionRange(0, 99999)
-    document.execCommand("copy");
-    textAreaDescript.value = ''
-})
+botaoCopiar.addEventListener('click', function () {
+	textAreaDescript.select();
+	textAreaDescript.setSelectionRange(0, 99999);
+	navigator.clipboard.writeText(textAreaDescript.value); //Usar ao invés do execCommand
+	textAreaDescript.value = '';
+});
